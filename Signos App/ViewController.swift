bb7f8a7c0b7d9e8a35ebcadf67de4e1214e0de91
HELIO MARCUS NERY FIDALGO <hmnff@hotmail.com>
2021-10-06 15:11:44 +0100

    Initial Commit

---
+++ Signos App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Signos App
+//
+//  Created by Helio Marcus Nery Fidalgo on 06/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
